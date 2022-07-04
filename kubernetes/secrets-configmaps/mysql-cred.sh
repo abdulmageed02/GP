d749697d52e6e6766b3bc1196545386dab5c108d
abdulmageed02 <76717091+abdulmageed02@users.noreply.github.com>
2022-07-04 21:35:39 +0200

    Update mysql-cred.sh

--- kubernetes/secrets-configmaps/mysql-cred.sh
+++ kubernetes/secrets-configmaps/mysql-cred.sh
@@ -7,5 +7,6 @@ metadata:
 data:
   MYSQL_DATABASE: `echo -n mydb | base64 -w0`
   MYSQL_ROOT_PASSWORD: `echo -n $PASS | base64 -w0`
-  MYSQL_USERNAME: `echo -n $USER | base64 -w0`
+  MYSQL_USER: echo -n $USER | base64 -w0
+  MYSQL_PASSWORD: echo -n $PASS | base64 -w0
 EOF
